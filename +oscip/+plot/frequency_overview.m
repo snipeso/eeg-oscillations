2cd9f96bc510dda150d1f243dfc2d4f4ebedb7d4
Sophia Snipes <snipeso@users.noreply.github.com>
2024-07-11 08:11:59 +0200

    plot correct axis limits

--- +oscip/+plot/frequency_overview.m
+++ +oscip/+plot/frequency_overview.m
@@ -25,8 +25,8 @@ figure('Units','centimeters', 'Position',[0 0 20 10], 'Color','w')
 subplot(1, 2, 1)
 oscip.plot.scoring_spectra(squeeze(mean(Power, 1, 'omitnan')), Frequencies, ...
     Scoring, ScoringIndexes, ScoringLabels, xLog, yLog)
-xlim([3 40])
+xlim([min(Frequencies) max(Frequencies)])
 
 subplot(1, 2, 2)
 oscip.plot.periodic_peaks(PeriodicPeaks, Scoring, ScoringIndexes, ScoringLabels, ScatterSizeScaling, Alpha)
-xlim([3 40])
+xlim([min(Frequencies) max(Frequencies)])
