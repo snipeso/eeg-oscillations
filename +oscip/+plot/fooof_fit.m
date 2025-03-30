8e1d0578a3e7e1d36bf6bfb7b5127b88b0081c7b
Sophia Snipes <snipeso@users.noreply.github.com>
2025-03-30 22:13:25 +0200

    little script to plot periodic peaks vs periodic power

---
+++ +oscip/+plot/fooof_fit.m
@@ -0,0 +1,17 @@
+function fooof_fit(PeriodicPower, PeriodicPeaks, FooofFrequencies)
+
+figure
+hold on
+plot(FooofFrequencies, PeriodicPower, 'LineWidth',1.5, 'Color','k')
+for PPIdx = 1:size(PeriodicPeaks, 1)
+    plot([PeriodicPeaks(PPIdx, 1), PeriodicPeaks(PPIdx, 1)], [0 PeriodicPeaks(PPIdx, 2)], 'Color', [1, 0 0], 'LineWidth',2)
+    
+    
+    Start= PeriodicPeaks(PPIdx, 1)-PeriodicPeaks(PPIdx, 3)/2;
+    End = PeriodicPeaks(PPIdx, 1)+PeriodicPeaks(PPIdx, 3)/2;
+    plot([Start, End], [PeriodicPeaks(PPIdx, 2)/2 PeriodicPeaks(PPIdx, 2)/2], 'Color', [.5 .5 .5], 'LineWidth',2)
+end
+xlabel('Frequency (Hz)')
+ylabel('Periodic power')
+set(gcf, 'Color','w')
+axis tight
