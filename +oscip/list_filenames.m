a95aa98bf6728063cc7fa89e677415e050399b9d
Sophia Snipes <snipeso@users.noreply.github.com>
2025-07-31 14:56:13 +0200

    add extension option

--- +oscip/list_filenames.m
+++ +oscip/list_filenames.m
@@ -1,10 +1,14 @@
-function Folder = list_filenames(Folder)
+function Folder = list_filenames(Folder, Extension)
 % little function for getting whatever is inside a folder, ignoring the
 % stupid dots and turning everything into a string
 
 Folder = deblank(string(ls(Folder)));
 
-Folder(strcmp(Folder, ".")) = [];
-Folder(strcmp(Folder, "..")) = [];
+if exist("Extension", 'var') && ~isempty(Extension)
+    Folder(~contains(Extension)) = [];
+else
+    Folder(strcmp(Folder, ".")) = [];
+    Folder(strcmp(Folder, "..")) = [];
 
-Folder(~contains(Folder, '.')) = [];
+    Folder(~contains(Folder, '.')) = [];
+end
