4195a7efad7151867000a11e10a27af71888be47
Sophia Snipes <snipeso@users.noreply.github.com>
2025-08-02 15:47:13 +0200

    comments and better default

--- +oscip/specparam_matlab.m
+++ +oscip/specparam_matlab.m
@@ -3,9 +3,16 @@ arguments
     Frequencies
     Power
     FittingFrequencyRange = [min(Frequencies), max(Frequencies)]
-    AdditionalParameters = oscip.default_settings()
+    AdditionalParameters = struct()
 end
-
+%  FooofModel = specparam_matlab(Frequencies, Power, FittingFrequencyRange, AdditionalParameters)
+%
+% This is just a little friendly wrapper for the specparam code, asking for
+% power and frequencies, running some checks, then running oscip.specparam.
+% It basically just skips having to figure out the specapram settings
+% structure.
+% 
+% By Sophia Snipes, 2025, for eeg-oscillations
 
 settings = oscip.sputils.check_settings(AdditionalParameters);
 settings.freq_range = FittingFrequencyRange;
