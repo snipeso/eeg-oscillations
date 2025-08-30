60569af81c15524dabff13043ae3164f7cb217a0
Sophia Snipes <snipeso@users.noreply.github.com>
2025-08-30 11:42:22 +0200

    minimum prominence

--- +oscip/find_closest_peak.m
+++ +oscip/find_closest_peak.m
@@ -1,6 +1,14 @@
-function Peak = find_closest_peak(Power, Frequencies, TargetFrequency)
+function Peak = find_closest_peak(Power, Frequencies, TargetFrequency, MinProminence)
+arguments
+    Power
+    Frequencies
+    TargetFrequency
+    MinProminence = .01
+end
 
 PeriodicPeaks = oscip.utils.findpeaks_matlab(Power, Frequencies, []);
+
+PeriodicPeaks(PeriodicPeaks(:, 4)<MinProminence, :) = [];
 
 Idx = dsearchn(PeriodicPeaks(:, 1), TargetFrequency);
 
