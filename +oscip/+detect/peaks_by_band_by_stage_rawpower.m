251b98b13923045fa0d73ae4bc53fcd794906004
Sophia Snipes <snipeso@users.noreply.github.com>
2025-12-04 09:18:55 +0100

    code version first checking prominance

--- +oscip/+detect/peaks_by_band_by_stage_rawpower.m
+++ +oscip/+detect/peaks_by_band_by_stage_rawpower.m
@@ -14,36 +14,33 @@ end
 
 if isempty(AdditionalParameters)
     AdditionalParameters = struct();
-    % AdditionalParameters.peak_width_limits = [0.5 20]; % not needed since specparam peaks not used?
     AdditionalParameters.min_peak_height = MinPeakProminance; % not strictly needed, but at least consistent
 end
 
 Dims = size(EpochPower);
-% if numel(Dims)==3
-%     EpochPower = squeeze(mean(EpochPower, 1, 'omitnan'));
-% else
+
 if numel(Dims)==2
     EpochPower = permute(EpochPower, [3 1 2]);
 end
 
 StagePower = oscip.utils.average_stages(EpochPower, Scoring, ScoringIndexes, MinEpochs);
+StagePower = oscip.smooth_spectrum_median(StagePower, Frequencies, 2);
+StagePower = oscip.smooth_spectrum(StagePower, Frequencies, 2);
 
 PeriodicStagePower = nan(Dims(1), numel(ScoringIndexes));
 PeaksTable = table();
 for ChannelIdx = 1:Dims(1)
-    for StageIdx = 5 %1:numel(ScoringIndexes)
+    for StageIdx = 1:numel(ScoringIndexes)
         [~, ~, FrequenciesPeriodic, ~, PeriodicPower]  = oscip.fit_fooof_matlab(...
             squeeze(StagePower(ChannelIdx, StageIdx, :)), Frequencies, FittingFrequencyRange, AdditionalParameters);
 
         % just for finding the peak frequency, adjust so that all values are
         % above 0
         AdjustedPower = PeriodicPower- min(quantile(PeriodicPower, .1), 0);
-        % [~, locs, ~, p] = findpeaks(AdjustedPower, FrequenciesPeriodic, 'MinPeakProminence', MinPeakProminance, 'MinPeakDistance', MinPeakDistance);
-        %
-        % w  =  oscip.detect.fwhm(FrequenciesPeriodic,  AdjustedPower, locs);
+
         [~, locs, w, p] = oscip.detect.peaks(AdjustedPower, FrequenciesPeriodic, MinPeakProminance, MinPeakDistance);
 
-        % % re-calculate amplitudes and bandwidths
+        % % re-calculate amplitudes
         pks = PeriodicPower(ismember(FrequenciesPeriodic, locs)); % no longer uses adjusted spectra, so that values are more comparable across channels later
 
         % figure
@@ -68,15 +65,41 @@ BandLabels = fieldnames(Bands);
 PeaksByStageByBand = nan(numel(ScoringIndexes), numel(BandLabels), 5);
 for StageIdx = 1:numel(ScoringIndexes)
     for BandIdx = 1:numel(BandLabels)
-        Range = Bands.(BandLabels{BandIdx});
 
-        Peaks = sortrows(PeaksTable(PeaksTable.Stage==ScoringIndexes(StageIdx) & ...
-            PeaksTable.Frequency >= Range(1) & PeaksTable.Frequency < Range(2), :), ...
-            {'Amplitude', 'Prominance', 'Channel'}, {'descend','descend', 'descend'});
-
-        if ~isempty(Peaks)
-            PeaksByStageByBand(StageIdx, BandIdx, :) = Peaks{1, 1:5};
-        end
+        Peaks = select_peaks_in_stage_band(PeaksTable, Bands.(BandLabels{BandIdx}), ScoringIndexes(StageIdx));
+        % Peaks = select_most_prominant_peaks_by_channel(Peaks);
+        PeaksByStageByBand(StageIdx, BandIdx, :) = select_largest_peak(Peaks);
     end
 end
+end
+
+function Peaks = select_peaks_in_stage_band(PeaksTable, Range, ScoringIndex)
+
+Peaks = PeaksTable(PeaksTable.Stage== ScoringIndex & ...
+    PeaksTable.Frequency >= Range(1) & PeaksTable.Frequency < Range(2), :);
+end
+
+
+function Peaks = select_most_prominant_peaks_by_channel(PeaksTable)
+
+Peaks = table();
+
+for ChannelIdx = unique(PeaksTable.Channel)'
+    PeaksChannel = sortrows(PeaksTable(PeaksTable.Channel==ChannelIdx, :), 'Prominance', 'descend');
+    Peaks = [Peaks; PeaksChannel(1, :)];
+end
+end
+
+function Peak = select_largest_peak(PeaksTable)
+
+
+if ~isempty(PeaksTable)
+    Peaks = sortrows(PeaksTable, 'Amplitude', 'descend');
+
+    Peak = table2array(Peaks(1, {'Frequency', 'Amplitude', 'Bandwidth', 'Prominance', 'Channel'}));
+else
+    Peak = nan(1, 5);
+end
+end
+
 
