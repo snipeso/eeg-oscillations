ed28d9704d5f0c2466a4725e69f1843574ba17bf
Sophia Snipes <snipeso@users.noreply.github.com>
2026-01-07 18:14:51 +0100

    Create bandtable2struct.m

---
+++ +oscip/+utils/bandtable2struct.m
@@ -0,0 +1,39 @@
+function CustomBands = bandtable2struct(Metadata, Range, Bands, ScoringLabels)
+% this function assumes you've run some version of the peak detection, and
+% there's columns in a metadata table called "W_PeakAmplitude_Delta" or
+% similar.
+arguments
+    Metadata
+    Range = 0.5;
+    Bands = [];
+    ScoringLabels = {};
+end
+
+    CustomBands = struct();
+
+if isempty(Bands)
+
+    ColumnNames = Metadata.Properties.VariableNames;
+    ColumnNames(~contains(ColumnNames, 'PeakFrequency')) = [];
+
+    BandLabels = unique(extractAfter(ColumnNames, 'PeakFrequency_'));
+else
+    BandLabels = fieldnames(Bands);
+end
+
+if isempty(ScoringLabels)
+    ColumnNames = Metadata.Properties.VariableNames;
+    ColumnNames(~contains(ColumnNames, 'PeakFrequency')) = [];
+
+    ScoringLabels = unique(extractBefore(ColumnNames, '_PeakFrequency'));
+end
+
+for RowIdx = 1:size(Metadata, 1)
+    for BandIdx  = 1:numel(BandLabels)
+        for ScoringIdx = 1:numel(ScoringLabels)
+
+            PeakFrequency = Metadata.([ScoringLabels{ScoringIdx}, '_PeakFrequency_', BandLabels{BandIdx}])(RowIdx);
+            CustomBands(RowIdx, ScoringIdx).(BandLabels{BandIdx}) = PeakFrequency + [-Range, Range];
+        end
+    end
+end
