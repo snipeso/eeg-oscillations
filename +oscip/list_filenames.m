e00222e5d089b92962c91def05a745c899f8afaf
Sophia Snipes <code@sophia.science>
2026-02-19 16:21:25 +0100

    hopeflly doesnt break important things

--- +oscip/list_filenames.m
+++ +oscip/list_filenames.m
@@ -1,11 +1,19 @@
 function Files = list_filenames(Folder, Extension)
 % little function for getting whatever is inside a folder, ignoring the
-% stupid dots and turning everything into a string
+% stupid dots and turning everything into a string. Provide '' to extension
+% if you want folders
+arguments
+    Folder
+    Extension = '.'
+end
 
 Files = deblank(string(ls(Folder)));
 
-if exist("Extension", 'var') && ~isempty(Extension)
+if exist("Extension", 'var') && ~strcmp(Extension, '.')
     Files(~contains(Files, Extension)) = [];
+elseif isempty(Extension) % folder
+ Files(contains(Files, '.')) = [];
+
 else
     Files(strcmp(Files, ".")) = [];
     Files(strcmp(Files, "..")) = [];
